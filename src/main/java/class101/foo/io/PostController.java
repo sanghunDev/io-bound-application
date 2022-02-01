a67732347b292c831341a00361edfc1840511595
sangHun <art9000@naver.com>
2022-02-01 14:43:38 +0900

    feat: db 대신 es 사용

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -3,9 +3,6 @@ package class101.foo.io;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,13 +10,8 @@ import java.util.List;
 @RestController
 public class PostController {
 
-    private static Integer PAGE_SIZE = 20;
-
     @Autowired
     PostRepository postRepository;
-
-    @Autowired
-    PostCacheService postCacheService;
 
     @Autowired
     Producer producer;
@@ -33,32 +25,12 @@ public class PostController {
         String jsonPost = objectMapper.writeValueAsString(post);
         producer.sendTo(jsonPost);
         return post;
-//        return postRepository.save(post);
-    }
-
-    // 2 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        if (page.equals(1)) {
-            return postCacheService.getFirstPostPage();
-        } else {
-            return postRepository.findAll(
-                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-    }
-
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
     }
 
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
-    public List<Post> findByContent(@RequestParam String content) {
-        return postRepository.findByContentContains(content);
+    public List<Post> findPostsByContent(@RequestParam String content) {
+        return postRepository.findByContent(content);
     }
-
 
 }
